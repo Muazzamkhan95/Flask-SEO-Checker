d8b7e9ce4144b006f32fb74edf7065a793c20bb6
Muazzam Ali Khan <muazzamkhan95@gmail.com>
2025-06-11 04:49:11 +0400

    Added gunicorn

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
+import os
+
 class Config:
-    SECRET_KEY = '545121578dsafsdfasf25sa4dfas21df54asdf12asdf54asd2fasdf4a2s0df'
+
+    SECRET_KEY = os.environ.get("SECRET_KEY", "545121578dsafsdfasf25sa4dfas21df54asdf12asdf54asd2fasdf4a2s0df")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "postgresql://flask_user:password@localhost:5433/flask_app"
-class ProdConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:pass@prod-host/prod_db'
